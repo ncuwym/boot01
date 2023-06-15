b4cd422a1941a6b25c8724c2951d769931eab047
ncuwym <ncuwym@qq.com>
2023-06-16 00:52:11 +0800

    first commit

---
+++ src/main/java/com/yuming/boot/MappApplication.java
@@ -0,0 +1,15 @@
+package com.yuming.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangyuming
+ */
+@SpringBootApplication
+public class MappApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MappApplication.class,args);
+    }
+
+}
---
+++ src/main/java/com/yuming/boot/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.yuming.boot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+//@ResponseBody
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+
+    public String handleHello() {
+             return "hello,spring boot";
+    }
+}
