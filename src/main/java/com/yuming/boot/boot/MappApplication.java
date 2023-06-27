83fbd3f864b4d6b0994bd7cddfdc16133ab0edce
ncuwym <ncuwym@qq.com>
2023-06-27 23:40:24 +0800

    every day study

--- src/main/java/com/yuming/boot/boot/MappApplication.java
+++ src/main/java/com/yuming/boot/boot/MappApplication.java
@@ -13,14 +13,14 @@ public class MappApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext run = SpringApplication.run(MappApplication.class, args);
 
-        int beanCount = run.getBeanDefinitionCount();
-        System.out.println(beanCount);
-
-        String[] names = run.getBeanDefinitionNames();
-        for (String name:names) {
-            System.out.println(name);
-
-        }
+//        int beanCount = run.getBeanDefinitionCount();
+//        System.out.println(beanCount);
+//
+//        String[] names = run.getBeanDefinitionNames();
+//        for (String name:names) {
+//            System.out.println(name);
+//
+//        }
         
     }
 
